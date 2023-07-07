c22383d9e0209d0e59ddff15da69a2e263dfa11d
yury-ff <yury.r@forkedfinance.xyz>
2023-07-07 11:48:52 +0100

    second commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -33,7 +33,7 @@ function Login() {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
-        credentials: "include",
+        withCredentials: "true",
       });
       setValues({ name: "", email: "", password: "" });
       showAlert({
