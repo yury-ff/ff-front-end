d1ca3ad44c7034ec58f2c2700b80e1934c62ff57
yury-ff <yury.r@forkedfinance.xyz>
2023-07-11 17:21:37 +0100

    second commit

--- src/context.js
+++ src/context.js
@@ -17,7 +17,9 @@ const AppProvider = ({ children }) => {
 
   const fetchUser = async () => {
     try {
-      const { data } = await axios.get(`${url}/api/v1/users/showMe`);
+      const { data } = await axios.get(`${url}/api/v1/users/showMe`, {
+        withCredentials: true,
+      });
       console.log(data.user);
       saveUser(data.user);
     } catch (error) {
