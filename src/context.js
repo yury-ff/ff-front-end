5d62f8c43fa623f572abfc87a42e9f4e9a53d9d8
yury-ff <yury.r@forkedfinance.xyz>
2023-07-12 21:56:56 +0100

    second commit

--- src/context.js
+++ src/context.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext(null);
-const url = "https://ff-server-4tm6.onrender.com";
+const url = "https://forkedfinance.xyz";
 
 const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
