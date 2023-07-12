881c8baa445be71459ae7a80fb0c7fed1c0ae4aa
yury-ff <yury.r@forkedfinance.xyz>
2023-07-12 14:16:30 +0100

    second commit

--- src/context.js
+++ src/context.js
@@ -24,7 +24,27 @@ const AppProvider = ({ children }) => {
         },
         withCredentials: true,
         credentials: "same-origin",
+        proxy: {
+          protocol: "https",
+          host: "ff-server-4tm6.onrender.com",
+          port: 10000,
+        },
       });
+
+      // const response = await fetch(`${url}/api/v1/users/showMe`, {
+      //   method: "GET", // *GET, POST, PUT, DELETE, etc.
+      //   mode: "cors", // no-cors, *cors, same-origin
+      //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      //   credentials: "same-origin", // include, *same-origin, omit
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     // 'Content-Type': 'application/x-www-form-urlencoded',
+      //   },
+      //   redirect: "follow", // manual, *follow, error
+      //   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      //   body: JSON.stringify(data), // body data type must match "Content-Type" header
+      // });
+
       console.log(data.user);
       saveUser(data.user);
     } catch (error) {
