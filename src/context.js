3d3c47643ff1bbc86bc5abb7c97f6cc9d4d44bc2
yury-ff <yury.r@forkedfinance.xyz>
2023-07-13 07:53:55 +0100

    second commit

--- src/context.js
+++ src/context.js
@@ -18,10 +18,10 @@ const AppProvider = ({ children }) => {
   const fetchUser = async () => {
     try {
       const { data } = await axios.get(`${url}/api/v1/users/showMe`, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-        },
+        // headers: {
+        //   "Access-Control-Allow-Origin": "*",
+        //   "Content-Type": "application/json",
+        // },
         withCredentials: true,
         // credentials: "same-origin",
       });
